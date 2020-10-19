1bda88988a288f2ea71a5f52bc57158df017c433
maisonthefabulous <73109250+maisonthefabulous@users.noreply.github.com>
2020-10-19 15:07:14 +0100

    Add files via upload

---
+++ ring.js
@@ -0,0 +1,9 @@
+.circle:after {
+  content : '';
+  position : absolute;
+  width : 300px;
+  height : 300px;
+  border : 20px solid #fff;
+  border-radius : 50%;
+  box-shadow : 0 0 30px #AF7AC5 , 0 0 30px #AF7AC5 inset;
+}
